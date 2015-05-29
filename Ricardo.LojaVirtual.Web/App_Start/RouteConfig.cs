53075818546a9cd6e8cbfe4698370ae62cb19326
rico3d <rico3d2001@gmmail.com>
2015-05-29 12:27:30 -0300

    Aula 12

--- Ricardo.LojaVirtual.Web/App_Start/RouteConfig.cs
+++ Ricardo.LojaVirtual.Web/App_Start/RouteConfig.cs
@@ -14,6 +14,15 @@ namespace Ricardo.LojaVirtual.Web
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+            name: null,
+            url: "Pagina{pagina}",
+            defaults: new
+            {
+                controller = "Vitrine",
+                action = "ListaProdutos"
+            });
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
